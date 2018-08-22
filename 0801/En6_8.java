011246afa2d95a4f3e7dd162e1b8e4276e65c4a1
s17003 <s17003@std.it-college.ac.jp>
2018-08-22 16:46:31 +0900

    0801 commit 2

--- 0801/En6_8.java
+++ 0801/En6_8.java
@@ -6,7 +6,7 @@ class En6_8 {
 
 	//--- 単純挿入ソート （番兵法：配列の先頭要素は空いている）---//
 	static void insertionSort(int[] a, int n) {
-		for (int i = 2; i < n; i++) {
+		for (int i = 2; i < n + 1; i++) {
 			int tmp = a[0] = a[i];
 			int j = i;
 			for ( ; a[j - 1] > tmp; j--)
@@ -23,15 +23,15 @@ class En6_8 {
 		int nx = stdIn.nextInt();
 		int[] x = new int[nx + 1];
 
-		for (int i = 1; i < nx; i++) {
+		for (int i = 1; i < nx + 1; i++) {
 			System.out.print("x[" + i + "]：");
 			x[i] = stdIn.nextInt();
 		}
 
-		insertionSort(x, nx + 1);				// 配列xを単純挿入ソート
+		insertionSort(x, nx);				// 配列xを単純挿入ソート
 
 		System.out.println("昇順にソートしました。");
-		for (int i = 1; i < nx; i++)
+		for (int i = 1; i < nx + 1; i++)
 			System.out.println("x[" + i + "]＝" + x[i]);
 	}
 }
